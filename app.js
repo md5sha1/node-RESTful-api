860feed634d4373ed47f8f2f423247422a955d07
md5sha1 <sharif777k@gmail.com>
2015-04-09 13:47:32 -0400

    app.js updated for mvc

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 /*Required Modules and Dependencies for our REST API and Server Configuration */
 
 var express = require('express'); //express framework
+var main = require('./main');
+var map = require('./controller/router');
 var logger = require ('morgan'); //http req logger middleware
 var bodyParser = require('body-parser'); //nodejs body parsing middleware
 var app = express();
@@ -12,9 +14,27 @@ app.use(bodyParser.json());//parse application/json
 
 //Connect to the DB
 var mongoose = require('mongoose');
+var db = mongoose.connection;
+db.on('error', console.error);
+db.on('open', function(){
+console.log('Connected to the remote DB ' + Date.now());
+var Profile = require('./app/models/profile');
+//use the Profile constructor to create instances of profiles documents in MongoDB 
+});
 mongoose.connect('mongodb://md5sha1:pass@novus.modulusmongo.net:27017/Pezib3ug'); //connecting to the mongo DB from modulus
 
-var Profile = require('./models/profile'); //data model
+var router = expresss.Router(); //uses the Router middleware object
+
+//load the middleware specific to this router for all req/res
+router.use(function(req, res, next){
+console.log('Router ready to take client requests: ' + Date.now());
+next();
+});
+router.get('/', main.index);
+app.use('/api', router); // use the router prefixed w/api for all requests
+
+//map route to Profile controller object
+map.mapRoute(router, profile);
 
 //Initialize the Server
 var port = process.env.PORT || 9000;
